f64c0385f875869ae4601d79388ebec91d70621f
Christian Garbs <mitch@cgarbs.de>
2016-06-29 22:13:25 +0200

    add POD

--- lib/vCard/AddressBook/Compare.pm
+++ lib/vCard/AddressBook/Compare.pm
@@ -1,16 +1,71 @@
 use strict;
 use warnings;
 package vCard::AddressBook::Compare;
-# ABSTRACT: compare two vCard::AddressBooks via full_name
-
-use Moo;
+# ABSTRACT: compare two vCard::AddressBooks for unique or duplicate entries
 
 use List::Compare;
 use MooX::Types::MooseLike::Base 'InstanceOf'; # TODO: check out Type::Tiny::Class
 use Scalar::Util 'blessed';
 
+use Moo;
+
+=head1 SYNOPSIS
+
+    my $ab1 = vCard::AddressBook->new()->load_file('some_file.vcf');
+    my $ab2 = vCard::AddressBook->new()->load_file('other_file.vcf');
+    
+    my $cmp = vCard::AddressBook::Compare->new( $ab1, $ab2 );
+    
+    for my $vcard ( @{ $cmp->unique }) {
+        # do something with the entries unique to $ab1
+    }
+
+    for my $vcard ( @{ $cmp->complement }) {
+        # do something with the entries unique to $ab2
+    }
+
+    for my $vcard ( @{ $cmp->intersection }) {
+        # do something with the entries contained in both $ab1 and $ab2
+    }
+
+=head1 DESCRIPTION
+
+This class takes two L<vCard::AddressBook> instances and compares the
+L<vCard>s contained within those address books to determine unique
+and/or duplicate entries.
+
+The key used for comparison is L<vCard/full_name>.
+
+=attr primary
+
+The primary L<vCard::AddressBook> to compare.
+
+Read-only.  Must be set in the constructor via C<< new( primary => $ab
+) >> where C<$ab> is an instance of L<vCard::AddressBook>.
+
+=cut
+
 has primary   => ( is => 'ro', required => 1, isa => InstanceOf['vCard::AddressBook'] );
+
+=attr secondary
+
+The secondary L<vCard::AddressBook> to compare.
+
+Read-only.  Must be set in the constructor via C<< new( secondary =>
+$ab ) >> where C<$ab> is an instance of L<vCard::AddressBook>.
+
+=cut
+
 has secondary => ( is => 'ro', required => 1, isa => InstanceOf['vCard::AddressBook'] );
+
+=method new( $primary, $secondary )
+
+Convenience constructor: When C<new()> is called with two scalars,
+they are used as L</primary> and L</secondary> address book.  It's a
+short form of C<< new( primary => $primary, secondary => $secondary )
+>>
+
+=cut
 
 sub BUILDARGS {
     my ( $class, @args ) = @_;
@@ -27,6 +82,44 @@ sub BUILDARGS {
     return { @args };
 }
 
+=method unique()
+
+Returns a reflist of all L<vCard>s that are contained in the
+L<#primary> address book, but not in the L<#secondary> address book.
+
+=cut
+
+sub unique {
+    my $self = shift;
+    return $self->_get_from_map( $self->_primary, $self->_lc->get_unique() );
+}
+
+=method complement()
+
+Returns a reflist of all L<vCard>s that are contained in the
+L<#secondary> address book, but not in the L<#primary> address book.
+
+=cut
+
+sub complement {
+    my $self = shift;
+    return $self->_get_from_map( $self->_secondary, $self->_lc->get_complement() );
+}
+
+=method intersection()
+
+Returns a reflist of all L<vCard>s that are contained in both the
+L<#primary> and L<#secondary> address books.
+
+=cut
+
+sub intersection {
+    my $self = shift;
+    return $self->_get_from_map( $self->_primary, $self->_lc->get_intersection() );
+}
+
+# mapped L<#primary>: key = vCard-ids, value = vCards
+
 has _primary => ( is => 'lazy' );
 
 sub _build__primary {
@@ -34,12 +127,16 @@ sub _build__primary {
     return $self->_map_from_ab( $self->primary );
 }
 
+# mapped L<#secondary>: key = vCard-ids, value = vCards
+
 has _secondary => ( is => 'lazy' );
 
 sub _build__secondary {
     my $self = shift;
     return $self->_map_from_ab( $self->secondary );
 }
+
+# List::Compare instance
 
 has _lc => ( is => 'lazy' );
 
@@ -50,20 +147,9 @@ sub _build__lc {
 			      [ keys %{ $self->_secondary } ] );
 }
 
-sub unique {
-    my $self = shift;
-    return $self->_get_from_map( $self->_primary, $self->_lc->get_unique() );
-}
-
-sub complement {
-    my $self = shift;
-    return $self->_get_from_map( $self->_secondary, $self->_lc->get_complement() );
-}
-
-sub intersection {
-    my $self = shift;
-    return $self->_get_from_map( $self->_primary, $self->_lc->get_intersection() );
-}
+# generates the id for a L<vCard>
+#  in:  vCard instance
+#  out: id scalar
 
 sub _id_from_vcard {
     my $self = shift;
@@ -71,11 +157,20 @@ sub _id_from_vcard {
     return $vcard->full_name;
 }
 
+# get multiple values from a map
+#  in:  hashref of map, list of keys
+#  out: listref of values
+
 sub _get_from_map {
     my $self = shift;
     my ($map, @keys) = (@_);
     return [ map { exists $map->{$_} ? $map->{$_} : () } @keys ];
 }
+
+# create a map of vcards from an AddressBook
+# the id is generated via L<#_id_from_vcard>
+#  in:  vCard::AddressBook instance
+#  out: hashref of map ( id scalar => vCard instance )
 
 sub _map_from_ab {
     my $self = shift;
